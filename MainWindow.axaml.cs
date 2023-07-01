0e7d229eef090174d597290bc88be4292b7fe048
Dmitry Mozgin <m0391n@gmail.com>
2023-07-01 17:04:47 +0300

    Add OpenGL example

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,4 +1,10 @@
+using System;
+using System.Diagnostics;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
 
 namespace RNGExperiments;
 
