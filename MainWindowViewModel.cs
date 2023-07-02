d81559533168edad26014e51ec8dbc918b041317
Dmitry Mozgin <m0391n@gmail.com>
2023-07-02 03:43:14 +0300

    Картинка хранится в Bitmap

--- MainWindowViewModel.cs
+++ MainWindowViewModel.cs
@@ -1,28 +1,105 @@
 using Avalonia.Media;
 using Avalonia.Controls;
 using Avalonia.Controls.Shapes;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+using System.Runtime.InteropServices;
+using Avalonia;
 
 namespace RNGExperiments;
 
 public class MainWindowViewModel {
 
-    Rectangle _rectangle;
+    Image _image;
 
-    public MainWindowViewModel(Rectangle rectangle) {
-        _rectangle = rectangle;
+    int _imageWidth = 300;
+
+    int _imageHeight = 300;
+
+    Color _imageColor = Colors.Black;
+
+    Bitmap _bitmap = null;
+
+    public MainWindowViewModel(Image image) {
+        _image = image;
+        UpdateImageSize();
     }
 
-    object? _selectedColorItem;
-
-    public object? SelectedColorItem {
-        get => _selectedColorItem;
+    public string ImageWidth {
+        get => _imageWidth.ToString();
         set {
-            _selectedColorItem = value;
-            
-            if (value is ContentControl control) {
-                var colorText = control.GetValue(ContentControl.ContentProperty)?.ToString();
-                _rectangle.Fill = new SolidColorBrush(Color.Parse(colorText ?? "Black"));
+            if (int.TryParse(value, out var v)) {
+                _imageWidth = v;
+                UpdateImageSize();
             }
         }
+    }
+
+    public string ImageHeight {
+        get => _imageHeight.ToString();
+        set {
+            if (int.TryParse(value, out var v)) {
+                _imageHeight = v;
+                UpdateImageSize();
+            }
+        }
+    }
+
+    public object SelectedColorItem {
+        set {            
+            if (value is ContentControl control) {
+                var colorText = control.GetValue(ContentControl.ContentProperty)?.ToString();
+                _imageColor = Color.Parse(colorText ?? "Black");
+                SetImage();
+            }
+        }
+    }
+
+    void UpdateImageSize() {
+        _image.Width = _imageWidth;
+        _image.Height = _imageHeight;
+        SetImage();
+    }
+
+    void SetImage() {
+        if (_bitmap != null) {
+            _bitmap.Dispose();
+        }
+
+        var pixelFormat = PixelFormat.Rgba8888;
+        var bpp = 4;
+        var rowBytes = _imageWidth * bpp;
+        var totalBytes = _imageHeight * rowBytes;
+        var address = Marshal.AllocHGlobal(totalBytes);
+
+        unsafe {
+            byte *p = (byte*)address.ToPointer();
+
+            for (int y = 0; y < _imageHeight; y++) {
+                for (int x = 0; x < _imageWidth; x++) {
+                    var index = (_imageHeight - y - 1) * _imageWidth * bpp + x * bpp;
+                    var color = GetColorAt(x, y);
+                    p[index + 0] = color.R;
+                    p[index + 1] = color.G;
+                    p[index + 2] = color.B;
+                    p[index + 3] = color.A;
+                }
+            }
+        }
+        
+        _image.Source = new Bitmap(
+            pixelFormat, 
+            AlphaFormat.Opaque,
+            address,
+            new PixelSize(_imageWidth, _imageHeight),
+            new Vector(96, 96), 
+            rowBytes
+            );
+
+        Marshal.FreeHGlobal(address);
+    }
+
+    Color GetColorAt(int x, int y) {
+        return _imageColor;
     }
 }
